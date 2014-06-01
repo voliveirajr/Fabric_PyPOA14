c8d276938362688d4a266e2689ccb4a3484254ad
Volmar Oliveira <volmar.oliveira.jr@gmail.com>
2014-06-01 17:16:53 -0300

    django rest framework sample project

--- Fabric_PyPOA14/settings.py
+++ Fabric_PyPOA14/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9*_=3pnr8+b%%7#tz16mjn%m8)crv3&aj3=1$rs)w$nf9qmfq4'
+SECRET_KEY = 'gcgc-7=noi=pw_1qfxm#9l27t3ru0@w*!^i8y6gl-s_c%@tga7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,11 +56,27 @@ WSGI_APPLICATION = 'Fabric_PyPOA14.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'database.sql',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
+    'PAGINATE_BY': 10
 }
 
 # Internationalization
--- Fabric_PyPOA14/urls.py
+++ Fabric_PyPOA14/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
+from users import views
 
-from django.contrib import admin
-admin.autodiscover()
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Fabric_PyPOA14.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
